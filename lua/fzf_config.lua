e61e55eff3fc8a416854c2f1a72169e94cfecdb8
loinguyen <loi.nguyen@employmenthero.com>
2021-07-05 17:52:31 +0700

    Config fzf

--- lua/fzf_config.lua
+++ lua/fzf_config.lua
@@ -1,7 +1,4 @@
 local u = require('utils')
 
-u.g['$FZF_DEFAULT_COMMAND'] = 'rg --files --hidden'
-u.g['$FZF_DEFAULT_OPTS'] = '--layout=reverse'
-
-u.nmap('<leader>ag', '<ESC>:Rg<SPACE>')
+u.nmap('<leader>ag', '<ESC>:Rg<CR>')
 u.nmap('<leader>fl', '<ESC>:BLines<CR>')
