3296e7ac70e2bcdf9ebbfb7a34b2512244f98088
loinguyen <loi.nguyen@employmenthero.com>
2021-07-09 15:26:18 +0700

    Update ale config

--- lua/ale_config.lua
+++ lua/ale_config.lua
@@ -12,7 +12,7 @@ _ale_fixer['rspec'] = {'remove_trailing_lines', 'trim_whitespace'};
 
 u.g.ale_fixer = _ale_fixer
 u.g.ale_disable_lsp = 1
-u.g.ale_fix_on_save = 0
+u.g.ale_fix_on_save = 1
 
 u.g.ale_lint_on_text_changed = 0
 u.g.ale_lint_on_enter = 1
