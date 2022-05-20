fe1c0c846cfd2477df2dcac19593f4883e539e76
Loi Nguyen <ducloi221@gmail.com>
2022-05-20 21:42:20 +0700

    Remove conflict mapping

--- lua/hop_config.lua
+++ lua/hop_config.lua
@@ -1,4 +1,3 @@
 require('hop').setup()
 
 vim.api.nvim_set_keymap('n', '<Tab>', "<cmd>lua require'hop'.hint_words()<cr>", {})
-vim.api.nvim_set_keymap('n', '<C-l>', "<cmd>lua require'hop'.hint_lines()<cr>", {})
