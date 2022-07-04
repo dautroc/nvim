a1733f23b5d0fa043d354f8f8ddbb26fdc281424
Loi Nguyen <ducloi221@gmail.com>
2022-07-04 09:41:15 +0700

    Add hop lines

--- lua/hop_config.lua
+++ lua/hop_config.lua
@@ -1,3 +1,4 @@
 require('hop').setup()
 
 vim.api.nvim_set_keymap('n', '<Tab>', "<cmd>lua require'hop'.hint_words()<cr>", {})
+vim.api.nvim_set_keymap('n', '<c-l>', "<cmd>lua require'hop'.hint_lines()<cr>", {})
