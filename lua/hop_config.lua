d0b5713b4f0b0a83bc567fdfe5bab5e538d1f8dd
Loi Nguyen <ducloi221@gmail.com>
2022-07-06 13:26:27 +0700

    Update hop binding

--- lua/hop_config.lua
+++ lua/hop_config.lua
@@ -2,3 +2,4 @@ require('hop').setup()
 
 vim.api.nvim_set_keymap('n', '<Tab>', "<cmd>lua require'hop'.hint_words()<cr>", {})
 vim.api.nvim_set_keymap('n', '<c-l>', "<cmd>lua require'hop'.hint_lines()<cr>", {})
+vim.api.nvim_set_keymap('v', '<c-l>', "<cmd>lua require'hop'.hint_lines()<cr>", {})
