0199d036e4962d06a64cde72b862dfc5dd1ff51c
Loi Nguyen <ducloi221@gmail.com>
2022-10-30 11:57:12 +0700

    Add mapping for dispatch

--- lua/vimtest_config.lua
+++ lua/vimtest_config.lua
@@ -4,6 +4,8 @@ u.map('n', '<leader>tn', ':TestNearest<CR>')
 u.map('n', '<leader>tf', ':TestFile<CR>')
 u.map('n', '<leader>ts', ':TestSuite<CR>')
 
+u.map('n', '<leader>o', ':Copen<CR>') -- Open dispatch test
+
 u.g.VimuxUseNearest = 0
 u.g['test#strategy'] = 'dispatch_background'
 u.g['test#ruby#rspec#executable'] = 'bundle exec rspec'
