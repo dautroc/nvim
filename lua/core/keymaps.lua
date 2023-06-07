dc1d74471e8c8a2f2a3d3748467289893d7007ad
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-07 23:01:21 +0700

    Add gitignore

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -33,12 +33,6 @@ keymap("v", "p", '"_dP', opts)
 keymap("n", "x", '"_x', opts)
 keymap("n", "X", '"_X', opts)
 
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
