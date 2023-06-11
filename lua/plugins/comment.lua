800dce3b778c4f5522db6e7e918ac9bf4eed4e17
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-11 12:09:07 +0700

    Update toogle comment nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,32 +1,51 @@
 return {
-  "numToStr/Comment.nvim",
-  dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
-  keys = {
-    { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
-    { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
-  },
-  opts = {
-    pre_hook = function(ctx)
-      -- Only calculate commentstring for tsx filetypes
-      if vim.bo.filetype == "typescriptreact" then
-        local U = require("Comment.utils")
+	"numToStr/Comment.nvim",
+	dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+	keys = {
+		{
+			"gc",
+			mode = { "n", "v" },
+			desc = "Comment toggle linewise",
+		},
+		{
+			"gb",
+			mode = { "n", "v" },
+			desc = "Comment toggle blockwise",
+		},
+		{
+			"<leader>/",
+			":lua require('Comment.api').toggle.linewise.current()<CR>",
+			desc = "Toggle comment",
+		},
+		{
+			"<leader>/",
+			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+			desc = "Toggle comment",
+      mode = "v",
+		},
+	},
+	opts = {
+		pre_hook = function(ctx)
+			-- Only calculate commentstring for tsx filetypes
+			if vim.bo.filetype == "typescriptreact" then
+				local U = require("Comment.utils")
 
-        -- Determine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
+				-- Determine whether to use linewise or blockwise commentstring
+				local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
 
-        -- Determine the location where to calculate commentstring from
-        local location = nil
-        if ctx.ctype == U.ctype.blockwise then
-          location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
+				-- Determine the location where to calculate commentstring from
+				local location = nil
+				if ctx.ctype == U.ctype.blockwise then
+					location = require("ts_context_commentstring.utils").get_cursor_location()
+				elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+					location = require("ts_context_commentstring.utils").get_visual_start_location()
+				end
 
-        return require("ts_context_commentstring.internal").calculate_commentstring({
-          key = type,
-          location = location,
-        })
-      end
-    end,
-  },
+				return require("ts_context_commentstring.internal").calculate_commentstring({
+					key = type,
+					location = location,
+				})
+			end
+		end,
+	},
 }
