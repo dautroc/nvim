7a784c7dc4aec3c6ffd4e5c6ed0284ffa6b5b0e0
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-13 21:03:20 +0700

    Update colorscheme

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,16 +1,2 @@
 vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_better_performance = 1
-
--- Set the background color based on the time of day
-local adapt_background = function()
-	if tonumber(os.date("%H")) < 12 then
-		return "light"
-	else
-		return "dark"
-	end
-end
-
--- Automatically update the background color when entering and leaving Vim
-vim.cmd([[
-  autocmd VimEnter * lua vim.opt.background = ]] .. adapt_background() .. [[
-]])
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,3 +1,4 @@
+vim.opt.background = "dark"                            -- tell vim what the background color looks like
 vim.opt.foldenable = false                             -- disable folding by default
 vim.opt.backup = false                                 -- creates a backup file
 vim.opt.clipboard = "unnamedplus"                      -- allows neovim to access the system clipboard
