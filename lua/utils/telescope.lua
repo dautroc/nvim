8fc4d1e04e36e7c78ec9e47c6535fe993cc78741
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-16 09:36:33 +0700

    Another approach nvimtree

--- lua/utils/telescope.lua
+++ lua/utils/telescope.lua
@@ -7,3 +7,23 @@ function M.grep_at_current_tree_node()
 end
 
 return M
+
+-- Another approach
+-- live grep using Telescope inside the current directory under
+-- the cursor (or the parent directory of the current file)
+-- local function grep_in(node)
+--   if not node then
+--     return
+--   end
+--   local path = node.absolute_path or uv.cwd()
+--   if node.type ~= 'directory' and node.parent then
+--     path = node.parent.absolute_path
+--   end
+--   require('telescope.builtin').live_grep({
+--     search_dirs = { path },
+--     prompt_title = string.format('Grep in [%s]', vim.fs.basename(path)),
+--   })
+-- end
+
+-- in the nvimtree `setup`
+-- { key = '<C-f>', action = '', action_cb = grep_at, mode = 'n' },
