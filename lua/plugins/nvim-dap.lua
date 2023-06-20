26c39e316c423e348a806e56ec9b850e27ca755a
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-20 22:06:29 +0700

    Add nvim-dap

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,56 @@
+return {
+	"mfussenegger/nvim-dap",
+	keys = {
+		{ "<leader>dt", "<CMD>lua require'dap'.toggle_breakpoint()<CR>", desc = "Toggle breakpoint" },
+		{ "<leader>dc", "<CMD>lua require'dap'.continue()<CR>", desc = "Continue" },
+		{ "<leader>do", "<CMD>lua require'dap'.step_over()<CR>", desc = "Step over" },
+		{ "<leader>di", "<CMD>lua require'dap'.step_info()<CR>", desc = "Step info" },
+		{ "<leader>dr", "<CMD>lua require'dap'.repl.open()<CR>", desc = "RELP open" },
+	},
+	config = function()
+		local dap = require("dap")
+		dap.adapters.ruby = function(callback, config)
+			callback({
+				type = "server",
+				host = "127.0.0.1",
+				port = "${port}",
+				executable = {
+					command = "bundle",
+					args = {
+						"exec",
+						"rdbg",
+						"-n",
+						"--open",
+						"--port",
+						"${port}",
+						"-c",
+						"--",
+						"bundle",
+						"exec",
+						config.command,
+						config.script,
+					},
+				},
+			})
+		end
+
+		dap.configurations.ruby = {
+			{
+				type = "ruby",
+				name = "debug current file",
+				request = "attach",
+				localfs = true,
+				command = "ruby",
+				script = "${file}",
+			},
+			{
+				type = "ruby",
+				name = "run current spec file",
+				request = "attach",
+				localfs = true,
+				command = "rspec",
+				script = "${file}",
+			},
+		}
+	end,
+}
