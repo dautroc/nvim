a3082643efb6d385e6d2e9e34f5a8fa2a76194ac
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-25 00:36:03 +0700

    Disable nvim tree

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,11 +1,12 @@
 return {
 	"echasnovski/mini.nvim",
 	keys = {
-		{ "<leader>.", "<CMD>lua MiniFiles.open()<CR>", desc = "Mini files" },
+		{ "<leader>e", "<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>", desc = "Mini file current" },
+		{ "<leader>E", "<CMD>lua MiniFiles.open()<CR>", desc = "Mini file root" },
 	},
 	version = false,
 	config = function()
-		require("mini.files").setup()
 		require("mini.splitjoin").setup()
+		require("mini.files").setup()
 	end,
 }
