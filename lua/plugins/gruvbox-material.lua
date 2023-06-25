a8020b44f8a890a24a99b3eb750971c2e8edf71e
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-25 12:04:24 +0700

    Remove theme mapping

--- lua/plugins/gruvbox-material.lua
+++ lua/plugins/gruvbox-material.lua
@@ -1,8 +1,4 @@
 return {
-	keys = {
-		{ "<leader>mtd", "<cmd>set background=dark<cr>", desc = "Set dark theme" },
-		{ "<leader>mtl", "<cmd>set background=light<cr>", desc = "Set light theme" },
-	},
 	"sainnhe/gruvbox-material",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
