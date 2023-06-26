f83945ae338adec89af278902797d8146d675d64
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-06-26 17:46:54 +0700

    Update mini split join

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
 	keys = {
 		{ "<leader>e", "<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>", desc = "Mini file current" },
 		{ "<leader>E", "<CMD>lua MiniFiles.open()<CR>", desc = "Mini file root" },
+		{ "gS", "<CMD>lua MiniSplitjoin.toggle()<CR>", desc = "Mini splitjoin" },
 	},
 	version = false,
 	config = function()
