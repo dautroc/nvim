16c5245f31c1e1b0d4f60684cc2fa3b83a895d38
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-07-03 09:40:03 +0700

    Add search replace

---
+++ lua/plugins/search-replace.lua
@@ -0,0 +1,15 @@
+return {
+  "roobert/search-replace.nvim",
+  keys = {
+    { "<leader>mso", "<cmd>SearchReplaceSingleBufferOpen<CR>", desc = "Open" },
+    { "<leader>msw", "<cmd>SearchReplaceSingleBufferCWord<CR>", desc = "CWord" },
+    { "<C-r>", "<cmd>SearchReplaceSingleBufferVisualSelection<CR>", desc = "Visual Selection", mode = "v" },
+  },
+  config = function()
+    require("search-replace").setup({
+      -- optionally override defaults
+      default_replace_single_buffer_options = "gcI",
+      default_replace_multi_buffer_options = "egcI",
+    })
+  end,
+}
