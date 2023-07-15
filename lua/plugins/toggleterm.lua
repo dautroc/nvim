434f19901f089bb9303bbc157a38c691ca64e164
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-07-15 22:35:47 +0700

    Add github dash

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -75,5 +75,35 @@ return {
 		--       desc = "ToggleTerm " .. i .. ""
 		-- 	})
 		-- end
+
+		local Terminal = require("toggleterm.terminal").Terminal
+		local gh_dash = Terminal:new({
+			cmd = "gh dash",
+			dir = "git_dir",
+			direction = "float",
+			float_opts = {
+				border = "double",
+			},
+			-- function to run on opening the terminal
+			on_open = function(term)
+				vim.cmd("startinsert!")
+				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			end,
+			-- function to run on closing the terminal
+			on_close = function(term)
+				vim.cmd("startinsert!")
+			end,
+		})
+
+		function _Github_dashboard_toggle()
+			gh_dash:toggle()
+		end
+
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>gd",
+			"<cmd>lua _Github_dashboard_toggle()<CR>",
+			{ noremap = true, silent = true, desc = "Github dashboard" }
+		)
 	end,
 }
