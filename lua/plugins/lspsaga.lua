b2191aac48f0531c48460c21e66dc6569ac1ac71
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-07-24 23:53:07 +0700

    Update lsp config

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,6 +1,17 @@
 return {
 	"nvimdev/lspsaga.nvim",
 	event = "LspAttach",
+	keys = {
+		{ "K", "<cmd>Lspsaga hover_doc<cr>", desc = "Lspsaga hover" },
+		{ "gr", "<cmd>Lspsaga finder ref+def<cr>", desc = "Lspsaga ref+def" },
+		{ "gR", "<cmd>Lspsaga rename<cr>", desc = "Lspsaga rename" },
+		{ "gD", "<cmd>Lspsaga goto_definition<cr>", desc = "Lspsaga goto definition" },
+		{ "gd", "<cmd>Lspsaga peek_definition<cr>", desc = "Lspsaga peek definition" },
+		{ "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "Lspsaga diagnosict next" },
+		{ "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "Lspsaga diagnosict prev" },
+		{ "gA", "<cmd>Lspsaga code_action<cr>", desc = "Lspsaga code action" },
+		{ "go", "<cmd>Lspsaga outline<cr>", desc = "Lspsaga outline" },
+	},
 	config = function()
 		require("lspsaga").setup({})
 	end,
