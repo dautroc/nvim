7d37e54f516d20ee18e8914d33801d816a935123
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-07-24 21:53:59 +0700

    Update lsp config

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -1,18 +1,13 @@
 return {
-  "nvimdev/guard.nvim",
-  keys = {
-    { "gf", ":GuardFmt<CR>", "Guard format" }
-  },
-  config = function()
-    local ft = require('guard.filetype')
-    -- use stylua to format lua files and no linter
-    ft('lua'):fmt('stylua')
+	"nvimdev/guard.nvim",
+	config = function()
+		local ft = require("guard.filetype")
+		-- use stylua to format lua files and no linter
+		ft("lua"):fmt("stylua")
 
-    -- use lsp to format first then use rubocop format
-    ft('ruby'):fmt('lsp')
-        :append('rubocop')
-        :lint('rubocop')
+		-- use lsp to format first then use rubocop format
+		ft("ruby"):fmt("lsp"):append("rubocop"):lint("rubocop")
 
-    require('guard').setup()
-  end,
+		require("guard").setup()
+	end,
 }
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,41 +1,46 @@
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v2.x",
-  dependencies = {
-    -- LSP Support
-    { "neovim/nvim-lspconfig" }, -- Required
-    {                            -- Optional
-      "williamboman/mason.nvim",
-      build = function()
-        pcall(vim.api.nvim_command, "MasonUpdate")
-      end,
-    },
-    { "williamboman/mason-lspconfig.nvim" }, -- Optional
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
+	dependencies = {
+		-- LSP Support
+		{ "neovim/nvim-lspconfig" }, -- Required
+		{ -- Optional
+			"williamboman/mason.nvim",
+			build = function()
+				pcall(vim.api.nvim_command, "MasonUpdate")
+			end,
+		},
+		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    -- Autocompletion
-    { "hrsh7th/nvim-cmp" },     -- Required
-    { "hrsh7th/cmp-nvim-lsp" }, -- Required
-    { "L3MON4D3/LuaSnip" },     -- Required
-  },
-  config = function()
-    local lsp = require("lsp-zero").preset({})
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" }, -- Required
+		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+		{ "L3MON4D3/LuaSnip" }, -- Required
+	},
+	config = function()
+		local lsp = require("lsp-zero").preset({})
 
-    lsp.on_attach(function(client, bufnr)
-      local opts = { buffer = bufnr, silent = true }
-      -- lsp.default_keymaps({ buffer = bufnr })
+		lsp.on_attach(function(client, bufnr)
+			local opts = { buffer = bufnr, silent = true }
+			-- lsp.default_keymaps({ buffer = bufnr })
 
-      vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-      vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-      vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-      vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-      vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-      vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-      vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-      vim.keymap.set("n", "gq", "<cmd>lua vim.lsp.buf.format({ async=true })<cr>", opts)
-      vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-      vim.keymap.set('n', 'gA', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    end)
+			-- vim.keymap.set("n", "gq", "<cmd>lua vim.lsp.buf.format({ async=true })<cr>", opts)
+			vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+			vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+			vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+			vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+			vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
 
-    lsp.setup()
-  end,
+			vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<cr>", opts)
+			vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
+			vim.keymap.set("n", "gl", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts)
+			vim.keymap.set("n", "gL", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts)
+			vim.keymap.set("n", "gA", "<cmd>Lspsaga code_action<cr>", opts)
+			vim.keymap.set("n", "gf", "<cmd>Lspsaga finder<cr>", opts)
+			vim.keymap.set("n", "go", "<cmd>Lspsaga outline<cr>", opts)
+			vim.keymap.set("n", "gq", "<cmd>GuardFmt<cr>", opts)
+		end)
+
+		lsp.setup()
+	end,
 }
