870aa9cc5219b7540ad573ea248e3387dd4a87b2
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-07-25 23:09:41 +0700

    Update rails config

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -2,11 +2,13 @@ return {
 	"tpope/vim-rails",
   event = "VeryLazy",
   keys = {
-    { "<leader>aa", "<cmd>:A<cr>", desc = "Open alternate file" },
-    { "<leader>av", "<cmd>:AV<cr>", desc = "Open alternate file in vsplit" },
-    { "<leader>as", "<cmd>:AS<cr>", desc = "Open alternate file in hsplit" },
+    { "<leader>aa", "<cmd>:A<cr>", desc = "Rails alternate file" },
+    { "<leader>av", "<cmd>:AV<cr>", desc = "Rails alternate file in vsplit" },
+    { "<leader>as", "<cmd>:AS<cr>", desc = "Rails alternate file in hsplit" },
 
     -- Related file
-    { "<leader>ar", "<cmd>:R<cr>", desc = "Open related file" },
+    { "<leader>ar", "<cmd>:R<cr>", desc = "Rails related file" },
+    { "<leader>al", "<cmd>:RV<cr>", desc = "Rails related file in vsplit" },
+    { "<leader>aj", "<cmd>:RS<cr>", desc = "Rails related file in hsplit" },
   }
 }
