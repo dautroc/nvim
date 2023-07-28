04ed03e3b717d4e14285a3d31547c2026208cf19
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-07-28 16:20:05 +0700

    Update test keymap

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,11 +1,12 @@
-vim.g['test#ruby#rspec#executable'] = 'bundle exec rspec'
-vim.g['test#strategy'] = 'toggleterm'
+vim.g["test#ruby#rspec#executable"] = "bundle exec rspec"
+vim.g["test#strategy"] = "toggleterm"
 
 return {
 	"vim-test/vim-test",
 	keys = {
-		{ "<leader>rT", "<cmd>TestFile<cr>", desc = "Test file" },
-    { "<leader>rt", "<cmd>TestNearest<cr>", desc = "Test nearest" }
+		{ "<leader>rtf", "<cmd>TestFile<cr>", desc = "Test file" },
+		{ "<leader>rtn", "<cmd>TestNearest<cr>", desc = "Test nearest" },
+		{ "<leader>rtl", "<cmd>TestLast<cr>", desc = "Test last" },
 	},
-  dependencies = { 'preservim/vimux' }
+	dependencies = { "preservim/vimux" },
 }
