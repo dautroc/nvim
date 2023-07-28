3864c4c064ffa16048ddeed1a0272031c7093b7a
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-07-28 16:35:51 +0700

    Update vim test

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,5 +1,5 @@
 vim.g["test#ruby#rspec#executable"] = "bundle exec rspec"
-vim.g["test#strategy"] = "toggleterm"
+vim.g["test#strategy"] = "dispatch"
 
 return {
 	"vim-test/vim-test",
@@ -7,6 +7,7 @@ return {
 		{ "<leader>rtf", "<cmd>TestFile<cr>", desc = "Test file" },
 		{ "<leader>rtn", "<cmd>TestNearest<cr>", desc = "Test nearest" },
 		{ "<leader>rtl", "<cmd>TestLast<cr>", desc = "Test last" },
+		{ "<leader>rtv", "<cmd>TestVisit<cr>", desc = "Test visit" },
 	},
 	dependencies = { "preservim/vimux" },
 }
