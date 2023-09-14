fe22c53561239b59ae115c98f986574f169069e3
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-09-14 10:34:55 +0700

    Update which key

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,42 +1,43 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	config = function()
-		local wk = require("which-key")
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  config = function()
+    local wk = require("which-key")
 
-		wk.register({
-			b = { name = "+buffer/bookmark" },
-			d = { name = "+devdocs" },
-			f = {
-				name = "+find",
-				f = "+find and replace",
-			},
-			r = {
-				name = "+rails",
-			},
-			g = { name = "+git" },
-			a = { name = "+alternative" },
-			m = {
-				name = "+misc",
-			},
-			t = { name = "+test" },
-			l = { name = "+layout" },
-			o = {
-				name = "+obsidian",
-			},
-			n = {
-				name = "+neorg",
-				i = {
-					name = "+insert",
-				},
-				f = {
-					name = "+find",
-				},
-k		},
-		}, { prefix = "<leader>" })
-	end,
+    wk.register({
+      b = { name = "+buffer/bookmark" },
+      d = { name = "+devdocs" },
+      c = { name = "+qfixlist" },
+      f = {
+        name = "+find",
+        f = "+find and replace",
+      },
+      r = {
+        name = "+rails",
+      },
+      g = { name = "+git" },
+      a = { name = "+alternative" },
+      m = {
+        name = "+misc",
+      },
+      t = { name = "+test" },
+      l = { name = "+layout" },
+      o = {
+        name = "+obsidian",
+      },
+      n = {
+        name = "+neorg",
+        i = {
+          name = "+insert",
+        },
+        f = {
+          name = "+find",
+        },
+      },
+      }, { prefix = "<leader>" })
+  end,
 }
