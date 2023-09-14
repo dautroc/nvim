38769fefaefd2647f18d58916a0f9db8952fcb32
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-09-14 10:29:41 +0700

    Update nvim tree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,8 +1,8 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  enabled = false,
+  enabled = true,
   keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+    { "<leader>E", "<cmd>NvimTreeFindFile<CR>", desc = "NvimTree find file" },
   },
   opts = {
     update_focused_file = {
