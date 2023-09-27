5a960a6140e0ccb739a2118f1982bb2e1e326375
Loi <loi.nguyen@employmenthero.com>
2023-09-27 17:53:23 +0700

    Disabled CR confirm

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -70,7 +70,7 @@ return {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
         }),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
