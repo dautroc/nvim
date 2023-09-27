1eee5315568e427f067dc4f95d5c9acd48469ffd
Loi <loi.nguyen@employmenthero.com>
2023-09-27 21:31:31 +0700

    A skilled tester uncovers the hidden stories of sotfware, revealing the narrative beneath the surface and illuminating the path to excellence

---
+++ lua/plugins/neo-test.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
+    "olimorris/neotest-rspec"
+  },
+	keys = {
+		{ "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "Test file" },
+		{ "<leader>tn", "<cmd>lua require('neotest').run.run()<cr>", desc = "Test nearest" },
+		{ "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>", desc = "Test attach" },
+		{ "<leader>to", "<cmd>Neotest output-panel<cr>", desc = "Test output" },
+		{ "<leader>tl", "<cmd>Neotest run last<cr>", desc = "Test last" },
+		{ "<leader>ts", "<cmd>Neotest summary<cr>", desc = "Test summary" },
+		{ "<leader>tx", "<cmd>require('neotest').run.stop()<cr>", desc = "Test stop" },
+	},
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-rspec")
+      },
+    })
+  end
+}
--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,13 +1,13 @@
 vim.g["test#ruby#rspec#executable"] = "bundle exec rspec"
-vim.g["test#strategy"] = "dispatch"
+vim.g["test#strategy"] = "neovim"
 
 return {
 	"vim-test/vim-test",
+  enabled = false,
 	keys = {
 		{ "<leader>tf", "<cmd>TestFile<cr>", desc = "Test file" },
 		{ "<leader>tn", "<cmd>TestNearest<cr>", desc = "Test nearest" },
 		{ "<leader>tl", "<cmd>TestLast<cr>", desc = "Test last" },
 		{ "<leader>tv", "<cmd>TestVisit<cr>", desc = "Test visit" },
 	},
-	-- dependencies = { "preservim/vimux" },
 }
