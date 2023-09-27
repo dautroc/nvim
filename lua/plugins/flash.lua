16d174373e580f5806a123d61343621124eb9d2d
Loi <loi.nguyen@employmenthero.com>
2023-09-27 19:50:29 +0700

    Add trailspace

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,10 +4,10 @@ return {
   opts = {
     modes = {
       search = {
-        enabled = false,
+        enabled = true,
       },
       char = {
-        enabled = false,
+        enabled = true,
       },
     },
   },
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,25 +1,28 @@
 return {
-	"echasnovski/mini.nvim",
-	keys = {
-		{ "<leader>e", "<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>", desc = "Mini file current" },
-		{ "gS", "<CMD>lua MiniSplitjoin.toggle()<CR>", desc = "Mini split join" },
-	},
-	version = false,
-	config = function()
-		require("mini.splitjoin").setup()
-		require("mini.files").setup({
-			mappings = {
-				close = "q",
-				go_in = "l",
-				go_in_plus = "<CR>",
-				go_out = "h",
-				go_out_plus = "H",
-				reset = "<BS>",
-				show_help = "g?",
-				synchronize = "w",
-				trim_left = "<",
-				trim_right = ">",
-			},
-		})
-	end,
+  "echasnovski/mini.nvim",
+  keys = {
+    { "<leader>e", "<CMD>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>", desc = "Mini file current" },
+    { "gS", "<CMD>lua MiniSplitjoin.toggle()<CR>", desc = "Mini split join" },
+    { "<leader>mt", "<CMD>lua MiniTrailspace.trim()<CR>", desc = "Mini Trim" },
+  },
+  event = "BufRead",
+  version = false,
+  config = function()
+    require("mini.trailspace").setup()
+    require("mini.splitjoin").setup()
+    require("mini.files").setup({
+      mappings = {
+        close = "q",
+        go_in = "l",
+        go_in_plus = "<CR>",
+        go_out = "h",
+        go_out_plus = "H",
+        reset = "<BS>",
+        show_help = "g?",
+        synchronize = "w",
+        trim_left = "<",
+        trim_right = ">",
+      },
+    })
+  end,
 }
