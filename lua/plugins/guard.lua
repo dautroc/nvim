49cb37cf294d16333dd4b5ce527aeada0592535a
Loi <loi.nguyen@employmenthero.com>
2023-10-13 16:55:42 +0700

    Add nvim guard collection

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -1,18 +1,21 @@
 return {
-	"nvimdev/guard.nvim",
-	keys = {
-		{ "gq", "<cmd>GuardFmt<cr>", "Guard format" },
-	},
-	config = function()
-		local ft = require("guard.filetype")
-		-- use stylua to format lua files and no linter
-		ft("lua"):fmt("stylua")
+  "nvimdev/guard.nvim",
+  dependencies = {
+    "nvimdev/guard-collection",
+  },
+  keys = {
+    { "gq", "<cmd>GuardFmt<cr>", "Guard format" },
+  },
+  config = function()
+    local ft = require("guard.filetype")
+    -- use stylua to format lua files and no linter
+    ft("lua"):fmt("stylua")
 
-		-- use lsp to format first then use rubocop format
-		ft("ruby"):fmt("lsp"):append("rubocop"):lint("rubocop")
+    -- use lsp to format first then use rubocop format
+    ft("ruby"):fmt("lsp"):append("rubocop"):lint("rubocop")
 
-		require("guard").setup({
-			fmt_on_save = false,
-		})
-	end,
+    require("guard").setup({
+      fmt_on_save = false,
+    })
+  end,
 }
