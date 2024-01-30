b220ce06fa08f0d2b7a8ffa793b00c8bbad43de2
Loi <loi.nguyen@employmenthero.com>
2024-01-30 11:38:18 +0700

    Jump last edit position

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -24,14 +24,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 -- Automatically close tab/vim when nvim-tree is the last window in the tab
-vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+-- vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+--
+-- vim.api.nvim_create_autocmd({ "VimResized" }, {
+-- 	callback = function()
+-- 		vim.cmd("tabdo wincmd =")
+-- 	end,
+-- })
 
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-	callback = function()
-		vim.cmd("tabdo wincmd =")
-	end,
-})
-
+-- Terminal window navigation
 vim.cmd("autocmd FileType markdown setlocal shiftwidth=2 tabstop=2 softtabstop=2")
 
 function _G.set_terminal_keymaps()
@@ -44,3 +45,12 @@ function _G.set_terminal_keymaps()
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+
+-- Automatically jump to last edit cursor position
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Open file at the last position it was edited earlier',
+  group = misc_augroup,
+  pattern = '*',
+  command = 'silent! normal! g`"zv'
+})
