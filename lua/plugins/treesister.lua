f651590cff67fa4d6539f3590e123dffcdd1ddf6
Loi <loi.nguyen@employmenthero.com>
2024-04-03 22:39:40 +0700

    Fix treesister

--- lua/plugins/treesister.lua
+++ lua/plugins/treesister.lua
@@ -1,41 +1,37 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPre",
-  dependencies = {
-    "RRethy/nvim-treesitter-endwise", -- Needed to prevent treesitter ident issue with Ruby: https://github.com/tree-sitter/tree-sitter-ruby/issues/230#issuecomment-1312403487
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- Using protected call
-    local status_ok, treesitter_config = pcall(require, "nvim-treesitter.configs")
-    if not status_ok then
-      return
-    end
+	"nvim-treesitter/nvim-treesitter",
+	event = "BufReadPre",
+	dependencies = {
+		"RRethy/nvim-treesitter-endwise", -- Needed to prevent treesitter ident issue with Ruby: https://github.com/tree-sitter/tree-sitter-ruby/issues/230#issuecomment-1312403487
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		-- Using protected call
+		local status_ok, treesitter_config = pcall(require, "nvim-treesitter.configs")
+		if not status_ok then
+			return
+		end
 
-    treesitter_config.setup({
-      ensure_installed = require("utils").parsers,
-      sync_install = false,
+		treesitter_config.setup({
+			ensure_installed = require("utils").parsers,
+			sync_install = false,
 
-      matchup = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true, disable = { "python", "css" } },
+			matchup = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true, disable = { "python", "css" } },
 
-      -- Integration with other plugins
-      autopairs = { -- require autopairs plugin
-        enable = true,
-      },
-      autotag = { -- require autotag plugin
-        enable = true,
-        filetypes = { "html", "xml", "eruby", "erb", "embedded_template" },
-      },
-      context_commentstring = { -- require ts-comment string plugin
-        enable = true,
-        enable_autocmd = false,
-      },
-    })
-  end,
+			-- Integration with other plugins
+			autopairs = { -- require autopairs plugin
+				enable = true,
+			},
+			autotag = { -- require autotag plugin
+				enable = true,
+				filetypes = { "html", "xml", "eruby", "erb", "embedded_template" },
+			},
+		})
+	end,
 }
