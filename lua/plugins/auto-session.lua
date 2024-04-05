a9104cb57e22173068ef7d04f5323fd6bd9882ba
Loi <loi.nguyen@employmenthero.com>
2024-04-06 00:02:47 +0700

    Add dashboard

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -20,7 +20,7 @@ return {
   },
   config = function()
     require("auto-session").setup {
-      auto_session_enable_last_session = true,
+      auto_session_enable_last_session = false,
       auto_session_use_git_branch = true,
       auto_session_suppress_dirs = { "~/workspace" },
 
--- lua/plugins/rayso.lua
+++ lua/plugins/rayso.lua
@@ -1,5 +1,6 @@
 return {
   'TobinPalmer/rayso.nvim',
+  enabled = false,
   cmd = { 'Rayso' },
   config = function()
     require('rayso').setup {}
