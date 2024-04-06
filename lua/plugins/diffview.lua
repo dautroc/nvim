5f44a40d190c850ca424b7d29e5443d3e7c4a1a0
Loi <loi.nguyen@employmenthero.com>
2024-04-06 08:22:25 +0700

    Add fugit 2

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,8 +1,14 @@
 return {
 	"sindrets/diffview.nvim",
+	cmd = {
+		"DiffviewFileHistory",
+		"DiffviewOpen",
+		"DiffviewToggleFiles",
+		"DiffviewFocusFiles",
+		"DiffviewRefresh",
+	},
 	keys = {
-		{ "<leader>gdd", "<cmd>DiffviewOpen development..head<CR>", desc = "Diffview with development" },
-		{ "<leader>gdm", "<cmd>DiffviewOpen master..head<CR>", desc = "Diffview with master" },
+		{ "<leader>gdo", "<cmd>DiffviewOpen<CR>", desc = "Diffview open" },
 		{ "<leader>gdq", "<cmd>DiffviewClose<CR>", desc = "Diffview close" },
 		{ "<leader>gdf", "<cmd>DiffviewFileHistory %<CR>", desc = "Diffview file history" },
 		{ "<leader>gdf", ":'<,'>DiffviewFileHistory<CR>", desc = "Diffview file history", mode = "v" },
