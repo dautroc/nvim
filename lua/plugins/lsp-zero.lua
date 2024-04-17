a67c72adeb7c8d5cf633a25c57469867ebeed02a
Loi <loi.nguyen@employmenthero.com>
2024-04-17 09:32:49 +0700

    Temp disable lsp zero default setup

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -32,7 +32,7 @@ return {
     require('mason-lspconfig').setup({
       ensure_installed = {},
       handlers = {
-        lsp_zero.default_setup,
+        -- lsp_zero.default_setup,
         lua_ls = function()
           local lua_opts = lsp_zero.nvim_lua_ls()
           require('lspconfig').lua_ls.setup(lua_opts)
