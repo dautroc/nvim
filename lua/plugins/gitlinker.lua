8e32c250766d48d97d2d605075cf914f1d9d4fbd
Loi <loi.nguyen@employmenthero.com>
2024-05-15 11:54:24 +0700

    Update gitlinker

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -7,12 +7,21 @@ return {
 			'<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
 			desc = "Get link to current buffer",
 			mode = "n",
+      silent = true,
 		},
 		{
 			"<leader>gl",
 			'<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
 			desc = "Open in browsers",
 			mode = "v",
+      silent = true,
+		},
+		{
+			"<leader>gr",
+			'<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+			desc = "Open repo in browsers",
+			mode = "n",
+      silent = true,
 		},
 	},
 	dependencies = {
