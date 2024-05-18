3f34d316e00222a3a4d4a74d8748f621c8322eb4
Loi <loi.nguyen@employmenthero.com>
2024-05-18 22:39:05 +0700

    Update new plugins

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,9 +1,9 @@
 vim.keymap.set("n", "gf", function()
-  if require("obsidian").util.cursor_on_markdown_link() then
-    return "<cmd>ObsidianFollowLink<CR>"
-  else
-    return "gf"
-  end
+	if require("obsidian").util.cursor_on_markdown_link() then
+		return "<cmd>ObsidianFollowLink<CR>"
+	else
+		return "gf"
+	end
 end, { noremap = false, expr = true })
 
 function newNoteInput()
@@ -29,7 +29,7 @@ return {
 		-- Action
 		{ "<leader>on", "<cmd>lua newNoteInput()<CR>", desc = "New node" },
 		{ "<leader>ot", "<cmd>ObsidianToday<CR>", desc = "Today note" },
-		{ "<leader>oo", "<cmd>ObsidianToggleCheckbox<CR>", desc = "Toggle checkbox" },
+		{ "<leader>oo", "<cmd>ObsidianOpen<CR>", desc = "Open in Obsidian" },
 
 		-- Link
 		{ "<leader>olf", "<cmd>ObsidianFollowLink vsplit<CR>", desc = "Follow link" },
@@ -59,7 +59,34 @@ return {
 			date_format = "%Y-%m-%d-%a",
 			time_format = "%H:%M",
 		},
-		mappings = {},
+		mappings = {
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		-- Optional, customize how note IDs are generated given an optional title.
+		---@param title string|?
+		---@return string
+		note_id_func = function(title)
+			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+			-- In this case a note with the title 'My new note' will be given an ID that looks
+			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+			local suffix = ""
+			if title ~= nil then
+				-- If title is given, transform it into valid file name.
+				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				-- If title is nil, just add 4 random uppercase letters to the suffix.
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return tostring(os.time()) .. "-" .. suffix
+		end,
 		ui = {
 			enable = true, -- set to false to disable all additional syntax features
 			update_debounce = 200, -- update delay after a text change (in milliseconds)
@@ -93,6 +120,23 @@ return {
 				ObsidianBlockID = { italic = true, fg = "#89ddff" },
 				ObsidianHighlightText = { bg = "#75662e" },
 			},
+		},
+		-- Specify how to handle attachments.
+		attachments = {
+			-- The default folder to place images in via `:ObsidianPasteImg`.
+			-- If this is a relative path it will be interpreted as relative to the vault root.
+			-- You can always override this per image by passing a full path to the command instead of just a filename.
+			img_folder = "assets/imgs", -- This is the default
+			-- A function that determines the text to insert in the note when pasting an image.
+			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+			-- This is the default implementation.
+			---@param client obsidian.Client
+			---@param path obsidian.Path the absolute path to the image file
+			---@return string
+			img_text_func = function(client, path)
+				path = client:vault_relative_path(path) or path
+				return string.format("![%s](%s)", path.name, path)
+			end,
 		},
 	},
 }
---
+++ lua/plugins/vim-table.lua
@@ -0,0 +1,6 @@
+return {
+	"dhruvasagar/vim-table-mode",
+	keys = {
+		{ "<leader>om", "<cmd>TableModeToggle<CR>", desc = "Table mode toggle" },
+	},
+}
