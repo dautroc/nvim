8c6b6213d2190f0d5e76a94296fa820bf734c508
Loi <loi.nguyen@employmenthero.com>
2024-05-23 22:16:28 +0700

    Update config

--- lua/plugins/intro.lua
+++ lua/plugins/intro.lua
@@ -1,6 +1,36 @@
-return { -- plugins.lua
-  "OXY2DEV/intro.nvim",
-  config = function()
-    require("intro").setup();
-  end
+return {
+	"OXY2DEV/intro.nvim",
+	config = function()
+		require("intro").setup({
+			preset = {
+				name = "nvim",
+				opts = { "animated" },
+			},
+			merge = false,
+
+			showStatusline = false,
+			shadaValidate = false,
+
+			anchors = {
+				position = "bottom",
+				corner = "▒",
+
+				textStyle = { bg = "#BAC2DE", fg = "#181825" },
+				cornerStyle = { bg = "#BAC2DE", fg = "#181825" },
+			},
+			pathModifiers = {},
+			openFileUnderCursor = "<leader><leader>",
+
+			components = {},
+			globalHighlights = {},
+
+			animations = {
+				delay = 0,
+				updateDelay = 200,
+
+				highlightBased = {},
+				textBased = {},
+			},
+		})
+	end,
 }
