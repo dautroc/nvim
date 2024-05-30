4962bcf031fe663a9ebe090b6654bf30ba4b6db8
Loi <loi.nguyen@employmenthero.com>
2024-05-30 14:34:51 +0700

    Add screenkey

---
+++ lua/plugins/screenkey.lua
@@ -0,0 +1,9 @@
+return {
+	"NStefan002/screenkey.nvim",
+	cmd = "Screenkey",
+	version = "*",
+	config = true,
+  keys = {
+    { "<leader>mr", "<CMD>Screenkey<CR>", desc = "Toggle Screenkey" },
+  },
+}
