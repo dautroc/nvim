9e19d0f5e6919c3c56d211827545767133ac035f
Loi <loi.nguyen@employmenthero.com>
2024-07-08 09:22:42 +0700

    Update better escape and spf

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -67,11 +67,18 @@ end
 vim.api.nvim_set_keymap("n", "<leader>ga", "<cmd>lua _gh_dash_toggle()<CR>", { noremap = true, silent = true })
 
 -- Serpl
-local Serpl = Terminal:new({ cmd = "serpl", hidden = true, direction = "float" })
-function _serpl_toggle()
-	Serpl:toggle()
+-- local Serpl = Terminal:new({ cmd = "serpl", hidden = true, direction = "float" })
+-- function _serpl_toggle()
+-- 	Serpl:toggle()
+-- end
+-- vim.api.nvim_set_keymap("n", "<leader>fa", "<cmd>lua _serpl_toggle()<CR>", { noremap = true, silent = true })
+
+-- Suprerfile
+local Spf = Terminal:new({ cmd = "spf .", hidden = true, direction = "float" })
+function _spf_toggle()
+	Spf:toggle()
 end
-vim.api.nvim_set_keymap("n", "<leader>fa", "<cmd>lua _serpl_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>E", "<cmd>lua _spf_toggle()<CR>", { noremap = true, silent = true })
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 -- -- -- TERMINAL WINDOW NAVIGATION -- -- -- --
--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,16 +1,38 @@
 return {
 	"max397574/better-escape.nvim",
 	config = function()
-		-- require("better_escape").setup({
-		-- 	mapping = { "jk" }, -- a table with mappings to use
-		-- 	timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-		-- 	clear_empty_lines = true, -- clear line after escaping if there is only whitespace
-		-- 	keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-		-- 	-- example(recommended)
-		-- 	-- keys = function()
-		-- 	--   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-		-- 	-- end,
-		-- })
-    require("better_escape").setup()
+		require("better_escape").setup({
+			timeout = vim.o.timeoutlen,
+			mappings = {
+				i = {
+					j = {
+						k = "<Esc>",
+						j = false
+					},
+				},
+				c = {
+					j = {
+						k = false,
+						j = false
+					},
+				},
+				t = {
+					j = {
+						k = false,
+						j = false
+					},
+				},
+				v = {
+					j = {
+						k = false,
+					},
+				},
+				s = {
+					j = {
+						k = false,
+					},
+				},
+			},
+		})
 	end,
 }
