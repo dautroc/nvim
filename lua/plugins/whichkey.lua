f6e66f515520743b2c10e3b0a14b342a2522332c
Loi <loi.nguyen@employmenthero.com>
2024-08-26 22:28:45 +0700

    Update config

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -18,10 +18,19 @@ return {
 			{ "<leader>gd", group = "diffview" },
 			{ "<leader>h", group = "help" },
 			{ "<leader>i", group = "terms" },
+
+			{ "<leader>j", group = "just help!" },
+			{ "<leader>ja", desc = "Ask" },
+			{ "<leader>jr", desc = "Refresh" },
+			{ "<leader>je", desc = "Edit" },
+			{ "<leader>jd", desc = "Debug" },
+			{ "<leader>jh", desc = "Hint" },
+
 			{ "<leader>l", group = "lsp" },
 			{ "<leader>lt", desc = "+trouble" },
 			{ "<leader>m", group = "misc" },
 			{ "<leader>mu", group = "utils" },
+			{ "<leader>n", group = "neo-tree" },
 			{ "<leader>o", group = "obsidian" },
 			{ "<leader>of", group = "find" },
 			{ "<leader>ol", group = "link" },
@@ -29,7 +38,6 @@ return {
 			{ "<leader>s", group = "session" },
 			{ "<leader>t", group = "test" },
 			{ "<leader>y", group = "yaml" },
-			{ "<leader>n", group = "neo-tree" },
 		})
 
 		wk.add({
