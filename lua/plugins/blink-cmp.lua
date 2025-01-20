4ee939f8c18e37001c38efbd09d2a6753ff0260d
Loi <loi.nguyen@employmenthero.com>
2025-01-20 21:58:22 +0700

    Update config

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -5,12 +5,31 @@ return {
 	opts = {
 		keymap = {
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
       ['<CR>'] = { 'accept', 'fallback' },
-			["<S-Tab>"] = { "select_prev", "fallback" },
-			["<Tab>"] = { "select_next", "fallback" },
+
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.snippet_active() then
+						return cmp.accept()
+					else
+						return cmp.select_and_accept()
+					end
+				end,
+				"snippet_forward",
+				"fallback",
+			},
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+
 			["<C-b>"] = { "scroll_documentation_up", "fallback" },
 			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 		},
+
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
