4c4cff033d60613a957a7a4aa9342989efdee06a
Loi <loi.nguyen@employmenthero.com>
2025-01-24 18:11:05 +0700

    Add code companion

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,35 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	keys = {
+		{ "<leader>jj", "<CMD>CodeCompanion<CR>", desc = "Code Companion" },
+		{ "<leader>jc", "<CMD>CodeCompanionChat<CR>", desc = "Code Companion Chat" },
+		{ "<leader>jd", ":CodeCompanionCmd ", desc = "Code Companion Cmd" },
+		{ "<leader>ja", "<CMD>CodeCompanionActions<CR>", desc = "Code Companion Actions", mode = { "n", "x" } },
+	},
+	config = function()
+		require("codecompanion").setup({
+			adapters = {
+				deepseek = function()
+					return require("codecompanion.adapters").extend("ollama", {
+						name = "deepseek",
+						schema = {
+							model = {
+								default = "deepseek-r1:latest",
+							},
+							num_ctx = {
+								default = 131072,
+							},
+							num_predict = {
+								default = -1,
+							},
+						},
+					})
+				end,
+			},
+		})
+	end,
+}
