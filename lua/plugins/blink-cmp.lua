bf723a3ef12e449ab354fe60b8d1e0f32af83221
Loi <loi.nguyen@employmenthero.com>
2025-01-27 10:28:38 +0700

    Update blink

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,9 +3,12 @@ return {
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "*",
 	opts = {
+		-- 'default' for mappings similar to built-in completion
+		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+		-- See the full "keymap" documentation for information on defining your own keymap.
 		keymap = {
-			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-e>"] = { "hide", "fallback" },
+			preset = "super-tab",
 			["<CR>"] = { "accept", "fallback" },
 
 			["<Tab>"] = {
@@ -20,30 +23,13 @@ return {
 				"fallback",
 			},
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-			["<Up>"] = { "select_prev", "fallback" },
-			["<Down>"] = { "select_next", "fallback" },
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-
-			-- ["<S-Tab>"] = { "select_prev", "fallback" },
-			-- ["<Tab>"] = { "select_next", "fallback" },
-
-			["<C-b>"] = { "scroll_documentation_up", "fallback" },
-			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 		},
-		-- completion = {
-		-- 	menu = {
-		-- 		auto_show = function(ctx)
-		-- 			return ctx.mode ~= "cmdline" or not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
-		-- 		end,
-		-- 	},
-		-- },
 
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
 		},
+
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 			per_filetype = {
