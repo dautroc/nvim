f1336fe0bc1861fdeecdda47ab3fd4da7173c48d
Loi <loi.nguyen@employmenthero.com>
2025-02-01 00:03:58 +0700

    chore: enhance codecompanion config

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -5,16 +5,18 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 	},
 	keys = {
-		{ "<leader>jc", "<CMD>CodeCompanionChat Toggle<CR>", desc = "Code Companion Chat" },
+		{ "<leader>jj", "<CMD>CodeCompanionChat Toggle<CR>", desc = "Code Companion Chat" },
 		{ "<leader>jg", ":CodeCompanionCmd ", desc = "Generate command" },
 
 		-- Visual
-		{ "<leader>jj", ":CodeCompanion", desc = "Code Companion", mode = { "v" } },
+		{ "<leader>jj", ":CodeCompanion /buffer ", desc = "Ask with selected code", mode = { "v" } },
+		{ "<leader>je", "<CMD>CodeCompanion /explain<CR>", desc = "Explain selected code", mode = { "v" } },
 		{ "<leader>ja", "<CMD>CodeCompanionActions<CR>", desc = "Code Companion Actions", mode = { "n", "v" } },
 		{ "<leader>js", "<CMD>CodeCompanionChat Add<CR>", desc = "Add selection to chat", mode = { "v" } },
 	},
 	config = function()
 		require("codecompanion").setup({
+			language = "English",
 			adapters = {
 				ollama = function()
 					return require("codecompanion.adapters").extend("ollama", {
@@ -37,12 +39,9 @@ return {
 				end,
 			},
 			strategies = {
-				-- chat = { adapter = "copilot" },
-				-- inline = { adapter = "copilot" },
-				-- agent = { adapter = "copilot" },
-				chat = { adapter = "ollama" },
-				inline = { adapter = "ollama" },
-				agent = { adapter = "ollama" },
+				chat = { adapter = "copilot" },
+				inline = { adapter = "copilot" },
+				agent = { adapter = "copilot" },
 			},
 			display = {
 				chat = {
@@ -50,9 +49,13 @@ return {
 					show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
 					separator = "─", -- The separator between the different messages in the chat buffer
 					show_references = true, -- Show references (from slash commands and variables) in the chat buffer?
-					show_settings = true, -- Show LLM settings at the top of the chat buffer?
+					show_settings = false, -- Show LLM settings at the top of the chat buffer?
 					show_token_count = true, -- Show the token count for each response?
 					start_in_insert_mode = false, -- Open the chat buffer in insert mode?
+					icons = {
+						pinned_buffer = " ",
+						watched_buffer = "👀 ",
+					},
 					diff = {
 						enabled = true,
 						close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
@@ -62,37 +65,6 @@ return {
 					},
 				},
 			},
-			-- prompt_library = {
-			-- 	["Docusaurus"] = {
-			-- 		strategy = "chat",
-			-- 		description = "Write documentation for me",
-			-- 		opts = {
-			-- 			index = 11,
-			-- 			is_slash_cmd = false,
-			-- 			auto_submit = false,
-			-- 			short_name = "docs",
-			-- 		},
-			-- 		references = {
-			-- 			{
-			-- 				type = "file",
-			-- 				path = {
-			-- 					"doc/.vitepress/config.mjs",
-			-- 					"lua/codecompanion/config.lua",
-			-- 					"README.md",
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 		prompts = {
-			-- 			{
-			-- 				role = "user",
-			-- 				content = [[I'm rewriting the documentation for my plugin CodeCompanion.nvim, as I'm moving to a vitepress website. Can you help me rewrite it?
-			--                    I'm sharing my vitepress config file so you have the context of how the documentation website is structured in the `sidebar` section of that file.cu
-			--                    I'm also sharing my `config.lua` file which I'm mapping to the `configuration` section of the sidebar.
-			--                  ]],
-			-- 			},
-			-- 		},
-			-- 	},
-			-- },
 		})
 	end,
 }
