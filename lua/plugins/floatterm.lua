36c7986a5889fec8474df30f3bcc844b449c10cc
Loi <loi.nguyen@employmenthero.com>
2025-07-20 19:06:14 +0700

    chore(floatterm): update config

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -3,4 +3,18 @@ return {
 	dependencies = "nvzone/volt",
 	opts = {},
 	cmd = "FloatermToggle",
+	keys = {
+		{
+			"<A-f>",
+			"<cmd>FloatermToggle<cr>",
+      mode = { "n", "t" },
+			desc = "Toggle Floaterm",
+		},
+    {
+      "<A-r>",
+      "<C-h>e",
+      mode = { "n", "t" },
+      desc = "Next Floaterm",
+    }
+	},
 }
