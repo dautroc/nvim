ba6786265494baddea38517e968d3aa65ded6d1e
Loi <loi.nguyen@employmenthero.com>
2025-07-21 11:24:37 +0700

    feat(claudecode): add claude code

--- lua/plugins/floatterm.lua
+++ lua/plugins/floatterm.lua
@@ -1,20 +1,34 @@
 return {
 	"nvzone/floaterm",
 	dependencies = "nvzone/volt",
-	opts = {},
+	opts = {
+		border = false,
+		size = { h = 100, w = 100 },
+
+		-- to use, make this func(buf)
+		mappings = { sidebar = nil, term = nil },
+
+		-- Default sets of terminals you'd like to open
+		terminals = {
+			{ name = "Terminal" },
+			-- cmd can be function too
+			{ name = "Terminal", cmd = "neofetch" },
+			-- More terminals
+		},
+	},
 	cmd = "FloatermToggle",
 	keys = {
 		{
 			"<A-f>",
 			"<cmd>FloatermToggle<cr>",
-      mode = { "n", "t" },
+			mode = { "n", "t" },
 			desc = "Toggle Floaterm",
 		},
-    {
-      "<A-r>",
-      "<C-h>e",
-      mode = { "n", "t" },
-      desc = "Next Floaterm",
-    }
+		{
+			"<A-r>",
+			"<C-h>e",
+			mode = { "n", "t" },
+			desc = "Next Floaterm",
+		},
 	},
 }
