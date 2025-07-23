3cfbb1ded5705134edc7f96f9ab579029cac4cc0
Loi <loi.nguyen@employmenthero.com>
2025-07-23 11:36:33 +0700

    feat(fyler) add fyler and remap yazi

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -5,18 +5,18 @@ return {
 	keys = {
 		-- 👇 in this section, choose your own keymappings!
 		{
-			"<leader>e",
+			"<leader>y",
 			"<cmd>Yazi<cr>",
 			desc = "Open yazi at the current file",
 		},
 		{
 			-- Open in the current working directory
-			"<leader>E",
+			"<leader>Y",
 			"<cmd>Yazi cwd<cr>",
 			desc = "Open the file manager in nvim's working directory",
 		},
 		{
-			"<c-e>",
+			"<c-y>",
 			"<cmd>Yazi toggle<cr>",
 			desc = "Resume the last yazi session",
 		},
