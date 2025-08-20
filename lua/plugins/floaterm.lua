87f7023173ef8daa6f8db6b398cf4915d1932e29
Loi <loi.nguyen@employmenthero.com>
2025-08-20 21:16:40 +0700

    chore(floaterm): smart toggle

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -4,26 +4,52 @@ return {
 		-- Floaterm options
 		vim.g.floaterm_shell = vim.o.shell
 		vim.g.floaterm_title = "floaterm: $1/$2"
-		vim.g.floaterm_wintype = "float" -- Changed to float for title visibility
-		vim.g.floaterm_width = 0.99
-		vim.g.floaterm_height = 0.3
-		vim.g.floaterm_position = "bottom"
+		vim.g.floaterm_wintype = "float"
+		vim.g.floaterm_width = 0.8
+		vim.g.floaterm_height = 0.8
+		vim.g.floaterm_position = "center"
 		vim.g.floaterm_borderchars = "─│─│┌┐┘└"
 		vim.g.floaterm_autoclose = 0
 		vim.g.floaterm_autohide = 0
 		vim.g.floaterm_autoinsert = true
 		vim.g.floaterm_titleposition = "left"
 
-		-- Keymaps (normal mode)
-		vim.keymap.set("n", "<A-n>", ":FloatermNew<CR>", { silent = true, desc = "Floaterm New" })
-		vim.keymap.set("n", "<A-v>", ":FloatermNew --wintype=vsplit --width=0.5<CR>", { silent = true, desc = "Floaterm Vsplit" })
-		vim.keymap.set(
-			"n",
-			"<A-x>",
-			":FloatermNew --wintype=split --height=0.3<CR>",
-			{ silent = true, desc = "Floaterm Split" }
-		)
-		vim.keymap.set("n", "<A-f>", ":FloatermNew --wintype=float<CR>", { silent = true, desc = "Floaterm Float" })
+		-- Smart toggle function
+		local function smart_toggle_terminal(name, options)
+			options = options or ""
+
+			-- Check if terminal with this name exists
+			local terminals = vim.fn["floaterm#buflist#gather"]()
+			local terminal_exists = false
+			local terminal_bufnr = nil
+
+			for _, bufnr in ipairs(terminals) do
+				local term_name = vim.fn.getbufvar(bufnr, "floaterm_name")
+				if term_name == name then
+					terminal_exists = true
+					terminal_bufnr = bufnr
+					break
+				end
+			end
+
+			if terminal_exists then
+				-- Terminal exists, toggle to it
+				vim.cmd("FloatermToggle " .. name)
+			else
+				-- Terminal doesn't exist, create new one with name
+				vim.cmd("FloatermNew --name=" .. name .. " " .. options)
+			end
+		end
+
+		-- Make function globally available
+		_G.smart_toggle_terminal = smart_toggle_terminal
+
+		-- Smart toggle keymaps
+		vim.keymap.set("n", "<A-s>", function() smart_toggle_terminal("split", "--wintype=split --height=0.3") end, { silent = true, desc = "Smart toggle split terminal" })
+		vim.keymap.set("n", "<A-v>", function() smart_toggle_terminal("vsplit", "--wintype=vsplit --width=0.5") end, { silent = true, desc = "Smart toggle vsplit terminal" })
+		vim.keymap.set("n", "<A-f>", function() smart_toggle_terminal("float", "--wintype=float --width=0.8 --height=0.8") end, { silent = true, desc = "Smart toggle float terminal" })
+
+		-- Original keymaps (normal mode)
 		vim.keymap.set("n", "<A-k>", ":FloatermPrev<CR>", { silent = true, desc = "Floaterm Prev" })
 		vim.keymap.set("n", "<A-j>", ":FloatermNext<CR>", { silent = true, desc = "Floaterm Next" })
 		vim.keymap.set("n", "<A-t>", ":FloatermToggle<CR>", { silent = true, desc = "Floaterm Toggle" })
@@ -38,61 +64,22 @@ return {
 			callback = function()
 				vim.keymap.set("t", "jk", [[<C-\><C-n>]], { buffer = true })
 
-        -- Window navigation
-        vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { buffer = true, silent = true, desc = "Move to left window" })
-        vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { buffer = true, silent = true, desc = "Move to bottom window" })
-        vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { buffer = true, silent = true, desc = "Move to top window" })
-        vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { buffer = true, silent = true, desc = "Move to right window" })
+				-- Window navigation
+				vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { buffer = true, silent = true, desc = "Move to left window" })
+				vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { buffer = true, silent = true, desc = "Move to bottom window" })
+				vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { buffer = true, silent = true, desc = "Move to top window" })
+				vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { buffer = true, silent = true, desc = "Move to right window" })
 
-        -- Floaterm management
-				vim.keymap.set(
-					"t",
-					"<A-n>",
-					"<C-\\><C-n>:FloatermNew<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm New" }
-				)
-				vim.keymap.set(
-					"t",
-					"<A-v>",
-					"<C-\\><C-n>:FloatermNew --wintype=vsplit --width=0.5<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm Vsplit" }
-				)
-				vim.keymap.set(
-					"t",
-					"<A-x>",
-					"<C-\\><C-n>:FloatermNew --wintype=split --height=0.3<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm Split" }
-				)
-				vim.keymap.set(
-					"t",
-					"<A-f>",
-					"<C-\\><C-n>:FloatermNew --wintype=float<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm Float" }
-				)
-				vim.keymap.set(
-					"t",
-					"<A-k>",
-					"<C-\\><C-n>:FloatermPrev<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm Prev" }
-				)
-				vim.keymap.set(
-					"t",
-					"<A-j>",
-					"<C-\\><C-n>:FloatermNext<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm Next" }
-				)
-				vim.keymap.set(
-					"t",
-					"<A-t>",
-					"<C-\\><C-n>:FloatermToggle<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm Toggle" }
-				)
-				vim.keymap.set(
-					"t",
-					"<A-d>",
-					"<C-\\><C-n>:FloatermKill<CR>",
-					{ buffer = true, silent = true, desc = "Floaterm Kill" }
-				)
+				-- Smart toggle in terminal mode
+				vim.keymap.set("t", "<A-s>", [[<C-\><C-n>:lua smart_toggle_terminal("split", "--wintype=split --height=0.3")<CR>]], { buffer = true, silent = true, desc = "Smart toggle split terminal" })
+				vim.keymap.set("t", "<A-v>", [[<C-\><C-n>:lua smart_toggle_terminal("vsplit", "--wintype=vsplit --width=0.3")<CR>]], { buffer = true, silent = true, desc = "Smart toggle vsplit terminal" })
+				vim.keymap.set("t", "<A-f>", [[<C-\><C-n>:lua smart_toggle_terminal("float", "--wintype=float --width=0.8 --height=0.8")<CR>]], { buffer = true, silent = true, desc = "Smart toggle float terminal" })
+
+				-- Original terminal mode keymaps
+				vim.keymap.set("t", "<A-k>", "<C-\\><C-n>:FloatermPrev<CR>", { buffer = true, silent = true, desc = "Floaterm Prev" })
+				vim.keymap.set("t", "<A-j>", "<C-\\><C-n>:FloatermNext<CR>", { buffer = true, silent = true, desc = "Floaterm Next" })
+				vim.keymap.set("t", "<A-t>", "<C-\\><C-n>:FloatermToggle<CR>", { buffer = true, silent = true, desc = "Floaterm Toggle" })
+				vim.keymap.set("t", "<A-d>", "<C-\\><C-n>:FloatermKill<CR>", { buffer = true, silent = true, desc = "Floaterm Kill" })
 			end,
 		})
 	end,
