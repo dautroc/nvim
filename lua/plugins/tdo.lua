56e589e93fce0573ce7adfe5863494bb24dadcf4
Loi <loi.nguyen@employmenthero.com>
2025-08-21 22:48:24 +0700

    21 Aug 22:48

--- lua/plugins/tdo.lua
+++ lua/plugins/tdo.lua
@@ -3,12 +3,22 @@ return {
 	dependencies = "2kabhishek/pickme.nvim",
 	cmd = { "Tdo" },
 	keys = {
+    -- Core
 		{ "<leader>nn", "<cmd>Tdo<cr>", desc = "Today's todo" },
 		{ "<leader>ne", "<cmd>Tdo entry<cr>", desc = "Today's entry" },
 		{ "<leader>nf", "<cmd>Tdo files<cr>", desc = "Today's entry" },
-		{ "<leader>ns", "<cmd>Tdo search<cr>", desc = "Seach note" },
+		{ "<leader>ns", "<cmd>Tdo find<cr>", desc = "Seach note" },
+		{ "<leader>nc", "<cmd>Tdo note<cr>", desc = "Create note" },
+		{ "<leader>ni", "<cmd>Tdo notes<cr>", desc = "Incomplete todo" },
+		{ "<leader>nt", "<cmd>Tdo toggle<cr>", desc = "Toggle todo" },
+
+    -- Navigation
+		{ "<leader>nh", "<cmd>Tdo yesterday<cr>", desc = "Yesterday's note" },
+		{ "<leader>nl", "<cmd>Tdo tomorrow<cr>", desc = "Tomorrow's note" },
 	},
   config = function()
-    require("tdo").setup()
+    require("tdo").setup({
+      add_default_keybindings = false,
+    })
   end,
 }
