77dff15b3a51b3401e1499ee62b7a961cee9e678
Loi <loi.nguyen@employmenthero.com>
2025-09-13 21:22:52 +0700

    chore(snacks): update keymap

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,6 +13,22 @@ return {
       enabled = true,
       timeout = 3000,
     },
+    zen = {
+      win = {
+        width = 0.6,
+      },
+      toggles = {
+        dim = false,
+        git_signs = true,
+        mini_diff_signs = false,
+        -- diagnostics = false,
+        -- inlay_hints = false,
+      },
+      show = {
+        statusline = true, -- can only be shown when using the global statusline
+        tabline = false,
+      },
+    },
     picker = {
       layout = {
         preset = "ivy"
@@ -41,9 +57,8 @@ return {
     -- Top Pickers & Explorer
     { "<leader><space>", function() Snacks.picker.smart() end,                                   desc = "Smart Find Files" },
     { "<leader>,",       function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
-    { "<leader>/",       function() Snacks.picker.grep() end,                                    desc = "Grep" },
     { "<leader>:",       function() Snacks.picker.command_history() end,                         desc = "Command History" },
-    { "<leader>n",       function() Snacks.picker.notifications() end,                           desc = "Notification History" },
+    -- { "<leader>n",       function() Snacks.picker.notifications() end,                           desc = "Notification History" },
     { "<leader>e",       function() Snacks.explorer() end,                                       desc = "File Explorer" },
     -- find
     { "<leader>fb",      function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
@@ -100,14 +115,14 @@ return {
     { "<leader>Z",       function() Snacks.zen.zoom() end,                                       desc = "Toggle Zoom" },
     { "<leader>.",       function() Snacks.scratch() end,                                        desc = "Toggle Scratch Buffer" },
     { "<leader>S",       function() Snacks.scratch.select() end,                                 desc = "Select Scratch Buffer" },
-    { "<leader>n",       function() Snacks.notifier.show_history() end,                          desc = "Notification History" },
+    -- { "<leader>n",       function() Snacks.notifier.show_history() end,                          desc = "Notification History" },
     { "<leader>bd",      function() Snacks.bufdelete() end,                                      desc = "Delete Buffer" },
     { "<leader>cR",      function() Snacks.rename.rename_file() end,                             desc = "Rename File" },
     { "<leader>gB",      function() Snacks.gitbrowse() end,                                      desc = "Git Browse",               mode = { "n", "v" } },
     { "<leader>gg",      function() Snacks.lazygit() end,                                        desc = "Lazygit" },
     { "<leader>un",      function() Snacks.notifier.hide() end,                                  desc = "Dismiss All Notifications" },
-    { "<c-/>",           function() Snacks.terminal() end,                                       desc = "Toggle Terminal" },
-    { "<c-_>",           function() Snacks.terminal() end,                                       desc = "which_key_ignore" },
+    -- { "<c-/>",           function() Snacks.terminal() end,                                       desc = "Toggle Terminal" },
+    -- { "<c-_>",           function() Snacks.terminal() end,                                       desc = "which_key_ignore" },
     { "]]",              function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference",           mode = { "n", "t" } },
     { "[[",              function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference",           mode = { "n", "t" } },
     {
