279c14c5e8a9e918d441e5bb1acdf4b8de44df7e
Loi <loi.nguyen@employmenthero.com>
2025-10-01 23:38:36 +0700

    ft(sidekick): add sidekick

---
+++ lua/plugins/sidekick.lua
@@ -0,0 +1,72 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    cli = {
+      mux = {
+        backend = "zellij",
+        enabled = true,
+      },
+    },
+  },
+  keys = {
+    {
+      "<tab>",
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require("sidekick").nes_jump_or_apply() then
+          return "<Tab>" -- fallback to normal tab
+        end
+      end,
+      expr = true,
+      desc = "Goto/Apply Next Edit Suggestion",
+    },
+    {
+      "<c-.>",
+      function()
+        require("sidekick.cli").focus()
+      end,
+      desc = "Sidekick Switch Focus",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ia",
+      function()
+        require("sidekick.cli").toggle({ focus = true })
+      end,
+      desc = "Sidekick Toggle CLI",
+      mode = { "n", "v" },
+    },
+    -- {
+    --   "<leader>ic",
+    --   function()
+    --     require("sidekick.cli").toggle({ name = "claude", focus = true })
+    --   end,
+    --   desc = "Sidekick Claude Toggle",
+    --   mode = { "n", "v" },
+    -- },
+    {
+      "<leader>ig",
+      function()
+        require("sidekick.cli").toggle({ name = "gemini", focus = true })
+      end,
+      desc = "Sidekick Gemini Toggle",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ic",
+      function()
+        require("sidekick.cli").toggle({ name = "copilot", focus = true })
+      end,
+      desc = "Sidekick Copilot Toggle",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ip",
+      function()
+        require("sidekick.cli").select_prompt()
+      end,
+      desc = "Sidekick Ask Prompt",
+      mode = { "n", "v" },
+    },
+  },
+}
